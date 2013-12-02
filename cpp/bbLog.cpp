798d723ed48c9953d0cb7bb159796f011ac1942a
BeagleBoneBlack <Andrew.Melim@gatech.edu>
2013-12-02 13:18:56 -0500

    Acurate timings via clock_gettime, nanosecond precision

--- cpp/bbLog.cpp
+++ cpp/bbLog.cpp
@@ -9,8 +9,12 @@
 #include <iostream>
 #include <unistd.h>
 #include <string>
+#include <time.h>
 
 #include "serial/ASIOSerialPort.h"
+
+// Forward declare diff function
+timespec diff(timespec start, timespec end);
 
 int main(int argc, char *argv[]){
 
@@ -20,6 +24,9 @@ int main(int argc, char *argv[]){
   }
     
   string nextLine;
+  time_t timer;
+  clock_t clockt;
+  timespec time1, time2, difft;
   std::cout << "Beginning logging: " << std::endl << std::endl;
 
   ofstream logFile;
@@ -28,6 +35,7 @@ int main(int argc, char *argv[]){
 
   ASIOSerialPort imu("/dev/ttyO2", 57600);
 
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
   while(1)
   {
     try
@@ -41,9 +49,26 @@ int main(int argc, char *argv[]){
     if (nextLine != "")
     {
       std::cout << nextLine << std::endl;
-      logFile << nextLine << std::endl;
+      //time(&timer); // Get current time; same as timer = time(NULL)
+      //clockt = clock();
+      clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+      difft = diff(time1, time2);
+      time1 = time2;
+      logFile << difft.tv_sec << " " << difft.tv_nsec << " " <<  nextLine << std::endl;
     }
   }
   return 0;
+}
+
+timespec diff(timespec start, timespec end){
+    timespec temp;
+      if ((end.tv_nsec-start.tv_nsec)<0) {
+        temp.tv_sec = end.tv_sec-start.tv_sec-1;
+        temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+      } else {
+        temp.tv_sec = end.tv_sec-start.tv_sec;
+        temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+      }
+    return temp;
 }
 
